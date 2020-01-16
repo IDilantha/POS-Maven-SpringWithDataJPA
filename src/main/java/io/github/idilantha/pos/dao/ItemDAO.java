9f923f24836089e603e181f78e6a43ba2a49caba
IDilantha <isurudilantha@gmail.com>
2020-01-16 17:35:17 +0530

    Revamp ItemDAO interface with @Query

--- src/main/java/io/github/idilantha/pos/dao/ItemDAO.java
+++ src/main/java/io/github/idilantha/pos/dao/ItemDAO.java
@@ -2,8 +2,10 @@ package io.github.idilantha.pos.dao;
 
 import io.github.idilantha.pos.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ItemDAO extends JpaRepository<Item, String> {
 
-    String getLastItemCode() throws Exception;
+    @Query(value = "SELECT code FROM Item ORDER BY code",nativeQuery = true)
+    String getLastItemCode();
 }
--- src/main/java/io/github/idilantha/pos/dao/OrderDAO.java
+++ src/main/java/io/github/idilantha/pos/dao/OrderDAO.java
@@ -3,10 +3,12 @@ package io.github.idilantha.pos.dao;
 
 import io.github.idilantha.pos.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderDAO extends JpaRepository<Order, Integer> {
 
-    int getLastOrderId() throws Exception;
+
+    int getLastOrderId();
 
     boolean existsByCustomerId(String customerId) throws Exception;
 
