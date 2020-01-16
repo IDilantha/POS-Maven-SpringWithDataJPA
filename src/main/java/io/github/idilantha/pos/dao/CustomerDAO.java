1341e120e1439c12db93c7b4f3d483d43d82ff4e
IDilantha <isurudilantha@gmail.com>
2020-01-16 17:32:20 +0530

    Revamp CustomerDAO interface with @Query

--- src/main/java/io/github/idilantha/pos/dao/CustomerDAO.java
+++ src/main/java/io/github/idilantha/pos/dao/CustomerDAO.java
@@ -2,9 +2,12 @@ package io.github.idilantha.pos.dao;
 
 import io.github.idilantha.pos.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerDAO extends JpaRepository<Customer, String> {
 
-    String getLastCustomerId() throws Exception;
+    @Query(value = "SELECT customerId FROM Customer ORDER BY customerId",nativeQuery = true)
+    String getLastCustomerId();
+
 
 }
