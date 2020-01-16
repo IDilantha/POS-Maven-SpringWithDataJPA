efb58feac529586e248755569cac39cde050fc90
IDilantha <isurudilantha@gmail.com>
2020-01-16 17:48:08 +0530

    Fix Error in CustomerDAO

--- src/main/java/io/github/idilantha/pos/dao/CustomerDAO.java
+++ src/main/java/io/github/idilantha/pos/dao/CustomerDAO.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerDAO extends JpaRepository<Customer, String> {
 
-    @Query(value = "SELECT customerId FROM Customer ORDER BY customerId",nativeQuery = true)
+    @Query(value = "SELECT customerId FROM Customer ORDER BY customerId DESC LIMIT 1",nativeQuery = true)
     String getLastCustomerId();
 
 
